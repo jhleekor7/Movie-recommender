c2d6f80da7cdb37d4b21b650598f1440454a4fdc
Lee Jihun <jhleekor77@naver.com>
2026-04-23 13:59:41 +0900

    movie.h operator

--- movie.h
+++ movie.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <iostream>
 using namespace std;
 
 class Movie {
@@ -8,25 +9,23 @@ private:
     string title;
     string genre;
     int year;
-
-    double totalRating; 
-    int ratingCount;    
+    double totalRating;
+    int ratingCount;
 
 public:
     Movie();
-
-    
     Movie(int id, const string& t, const string& g, int y);
 
-    
     void addRating(double r);
     double getAverageRating() const;
-
     void display() const;
 
-    
     int getId() const;
     string getTitle() const;
     string getGenre() const;
     int getYear() const;
+
+    bool operator<(const Movie& other) const;
+    bool operator==(const Movie& other) const;
+    friend ostream& operator<<(ostream& os, const Movie& m);
 };
